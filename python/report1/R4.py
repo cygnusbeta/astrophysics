8f96c6fc495a6598fb3f707b96099eda05c0a8f4
cygnusbeta <betacygnib+git@gmail.com>
2019-11-16 05:51:57 +0900

    Add separated regresion line

--- python/report1/R4.py
+++ python/report1/R4.py
@@ -44,19 +44,48 @@ def main():
     for i, label in enumerate(labels):
         plt.text(1.1 * x[i], 0.9 * y[i], label, color="black", fontsize=9)
 
-    gradient, intercept, r_value, p_value, std_err = stats.linregress(np.log10(x), np.log10(y))
-    mn = np.min(x)
-    mx = np.max(x)
-    x1 = np.linspace(mn, mx, 500)
-    y1 = x1 ** gradient * 10 **intercept
-    plt.plot(x1, y1, '-r')
+    x1 = [
+        [
+            2.4397e3,
+            6.0518e3,
+            6.3710e3,
+            3.3895e3
+        ],
+        [
+            6.9911e4,
+            5.8232e4,
+            2.5362e4,
+            2.4622e4
+        ]
+    ]
+    y1 = [
+        [
+        3.3010e23,
+        4.8673e24,
+        5.9722e24,
+        6.4169e23
+        ],
+        [
+            1.8981e27,
+            5.6832e26,
+            8.6810e25,
+            1.0241e26
+        ]
+    ]
+    for l in range(2):
+        gradient, intercept, r_value, p_value, std_err = stats.linregress(np.log10(x1[l]), np.log10(y1[l]))
+        mn = np.min(x1[l])
+        mx = np.max(x1[l])
+        _x = np.linspace(mn, mx, 500)
+        _y = _x ** gradient * 10 **intercept
+        plt.plot(_x, _y, '-r')
+
+        print('gradient', l, gradient)
+        print('intercept', l, intercept)
 
     plt.xlabel('Radius (km)')
     plt.ylabel('Mass (kg)')
     plt.show()
-
-    print('gradient', gradient)
-    print('intercept', intercept)
 
 
 if __name__ == "__main__":
